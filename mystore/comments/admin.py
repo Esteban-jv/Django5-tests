9e6ee32e7090b69dd9ffe99be2ed9bba91834456
Esteban-jv <ejv.developer@gmail.com>
2024-06-25 15:59:53 -0500

    Elements and Comment app relationships registered

--- mystore/comments/admin.py
+++ mystore/comments/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Comment
+
+# Personalizar opciones de consulta en django admin
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text') # Campos que se muestran en la tabla
+    search_fields = ('id', 'text') # Filtros de búsqueda
+    date_hierarchy = 'posted' # Prioridad de filtros
+    ordering = ('posted',) # Se puede ordenaar por más de 1 elemento en la tabla
+    list_filter = ('id', 'posted') # Columna lateral de filtros
+    # list_editable = ('text',) # Para editar fuera de la tabla
+
+    fields = ('text',) # para modificar unicamente estos campos
+    # exclude = ('emenet',) # Opposite of fields
 
 # Register your models here.
+# admin.site.register(Comment, CommentAdmin) # Se puede hacer así o bien con el decorador de clase
--- mystore/element/admin.py
+++ mystore/element/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Type, Element
 
 # Register your models here.
+@admin.register(Category, Type, Element)
+class BaseAdmin(admin.ModelAdmin): #Se puede registrar todo en la misma clase!
+    list_display = ('id', 'title')
+    search_fields = ('title', 'slug')
+'''
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title', 'slug')
+
+@admin.register(Element)
+class ElementAdminAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title', 'slug')
+'''
